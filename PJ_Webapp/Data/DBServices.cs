7ce78d4f45e9bb6b2aad136f6caf1961eb6de665
rBrink21 <brinkrrr@gmail.com>
2024-01-18 16:22:01 -0600

    Added Delete functionality and skills now reading in properly

--- PJ_Webapp/Data/DBServices.cs
+++ PJ_Webapp/Data/DBServices.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using PJ_Webapp.Models;
 
 namespace PJ_Webapp.Data;
@@ -16,8 +17,19 @@ public class DBServices
         foreach (Skill skill in soldier.skills)
         {
             skill.skillId = Guid.NewGuid();
+            skill.soldierId = soldier.soldierId;
             dataContext.skills.Add(skill);
         }
+        dataContext.SaveChanges();
+    }
+    public List<Soldier> GetSoldiers()
+    {
+        return dataContext.soldiers.Include(s => s.skills).ToList();
+    }
+
+    public void DeleteSoldier(Soldier soldier)
+    {
+        dataContext.soldiers.Remove(soldier);
         dataContext.SaveChanges();
     }
 }
